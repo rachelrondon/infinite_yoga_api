79db824188888a94de76d1d25d8c2c2b91858fd9
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-04-04 19:38:22 -0400

    remove test line from resoures

--- resources.js
+++ resources.js
@@ -4,6 +4,4 @@ router.use('/moves', require('./controllers/moves_controllers'));
 
 router.use('/routines', require('./controllers/routines_controllers'));
 
-router.use('/', (req, res) => res.json({message: 'yep'}))
-
 module.exports = router;
