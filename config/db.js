20d24c56f9ace31f226b85686924f6f8157b3ee2
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-04-04 16:30:03 -0400

    update for deployment

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
 
-const db = pgp({
+const db = pgp(process.env.DATABASE_URL || {
   host: 'localhost',
   port: 5432,
   database: 'api_yoga'
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log('Server listening on port', PORT));
 
